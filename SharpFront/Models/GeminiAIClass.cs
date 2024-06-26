9d9988028d6faf382351068264a9836a952f4b86
devmehmetakifv <go4webb@gmail.com>
2024-06-26 14:17:55 +0300

    Removed inline Prompt class from GeminiAIClass and created a new model in Models directory for it.

--- SharpFront/Models/GeminiAIClass.cs
+++ SharpFront/Models/GeminiAIClass.cs
@@ -57,12 +57,4 @@ namespace SharpFront.Models
         }
        
     }
-    public class Prompt
-    {
-        public string Content { get; set; }
-        public Prompt(string _content)
-        {
-            Content = _content;
-        }
-    }
 }
---
+++ SharpFront/Models/Prompt.cs
@@ -0,0 +1,11 @@
+﻿namespace SharpFront.Models
+{
+    public class Prompt
+    {
+        public string Content { get; set; }
+        public Prompt(string _content)
+        {
+            Content = _content;
+        }
+    }
+}
